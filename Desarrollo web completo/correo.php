a71e6517ed8a7515e406985763e4b592e8988c0d
Soporte-Sistemas-gdl <83970623+Soporte-Sistemas-gdl@users.noreply.github.com>
2021-06-04 15:15:37 -0500

    contacto.php

---
+++ Desarrollo web completo/correo.php
@@ -0,0 +1,17 @@
+<?php
+$destinario = 'JPerez.N@tomza.com';
+$asunto = 'Contacto página web jalisco';
+//correo al que llegara el mensaje
+$nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$mensaje = $_POST['mensaje'];
+
+
+$header = "Mensaje enviado desde la página de tomza jalisco";
+$mensajecompleto = $nombre . "\n" . $telefono . "\n" . $correo . "\n" . $mensaje;
+
+mail($destinario, $asunto, $header, $mensajecompleto );
+echo "<script>alert('Correo enviado exitosamente')</script>";
+echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+?>
