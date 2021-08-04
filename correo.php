c08c72c06ee3d466b66b7823cf03d2272e534b18
Soporte-Sistemas-gdl <83970623+Soporte-Sistemas-gdl@users.noreply.github.com>
2021-08-04 16:20:47 -0500

    Update correo.php

--- correo.php
+++ correo.php
@@ -5,9 +5,7 @@ $correo = $_POST['correo'];
 $mensaje = $_POST['mensaje'];
 
 $header = 'From: ' . $correo . "\r\n";
-$header .= "X-Mailer: PHP/" . phpversion(). "\r\n";
-$header .= "Mime-Version: 1.0 \r\n";
-$header .= "Content-Type: text/plain";
+
 
 $message = "Este mensaje fue enviado por " . $nombre . ",\r\n";
 $message .= "Su correo es: " . $correo . " \r\n";
@@ -15,10 +13,10 @@ $message .= "Telefono :" . $telefono . " \r\n";
 $message .= "Mensaje: " . $_POST['mensaje'] . "\r\n";
 $message .= "Enviado el " . date('d/m/Y', time());
 
-$to = 'soportesistemas.jalisco@tomza.com';
+$to = 'eoy0907@gmail.com';
 $subject = 'Contacto página web jalisco';
 
-mail($to, $subject, utf8_decode($message), $header);
+mail($to, $subject, $message, $header);
 echo'<script type="text/javascript">
         alert("Mensaje enviado");
         window.location.href="/";
