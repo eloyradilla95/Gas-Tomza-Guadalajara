8dde793a0185efe9d103abfa478b83e1a4d001f3
eloyradilla95 <79443618+eloyradilla95@users.noreply.github.com>
2021-11-18 17:07:04 -0600

    upload
    script de animaciones para la página principal

---
+++ js/animacion-index.js
@@ -0,0 +1,14 @@
+let upload = document.querySelectorAll(".upload");
+
+function mostrarScroll(){
+    let scrollTop = document.documentElement.scrollTop;
+    for(var i=0; i < upload.length; i++){
+        let alturaupload = upload[i].offsetTop;
+        if(alturaupload - 700 < scrollTop){
+            upload[i].style.opacity = 1;
+            upload[i].classList.add("show_me_above")
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
