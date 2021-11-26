05980848cba53a9871588208de71ad000c286adb
eloyradilla95 <79443618+eloyradilla95@users.noreply.github.com>
2021-11-26 17:20:17 -0600

    efecto de nieve height

--- js/animacion-index.js
+++ js/animacion-index.js
@@ -4,7 +4,7 @@ function mostrarScroll(){
     let scrollTop = document.documentElement.scrollTop;
     for(var i=0; i < upload.length; i++){
         let alturaupload = upload[i].offsetTop;
-        if(alturaupload - 500 < scrollTop){
+        if(alturaupload - 600 < scrollTop){
             upload[i].style.opacity = 1;
             upload[i].classList.add("show_me_above")
         }
